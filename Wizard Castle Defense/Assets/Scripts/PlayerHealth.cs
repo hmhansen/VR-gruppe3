c669a39a1f80674efb9815f097e6fa4f02b73b5a
hmhansen <hanhak14@student.westerdals.no>
2016-11-27 08:48:49 +0100

    HealthBar added. Changes made in PlayerHealth to support this.

--- Wizard Castle Defense/Assets/Scripts/PlayerHealth.cs
+++ Wizard Castle Defense/Assets/Scripts/PlayerHealth.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
     public int startingHealth = 100;
     public int currentHealth;
+
+    public GameObject healthBar;
 
     public MeshRenderer mesh; //refers to the tower's mesh renderer
     public Color flashColor = new Vector4(255,0,0,0); //the color it changes to when it takes damage
@@ -53,6 +56,16 @@ public class PlayerHealth : MonoBehaviour {
 
         currentHealth -= amount;
 
+
+
+        //Health for healthbar localscale
+        float cur_Health = currentHealth;
+        cur_Health -= 2f;
+        float healthbarHP = cur_Health / 100;
+        Debug.Log("Hello" + healthbarHP);
+        // Set the health bar's value to the current health.
+        healthBar.transform.localScale = new Vector3(Mathf.Clamp(healthbarHP, 0f, 1f), healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+
         gameController.SetCastleIntegrity(currentHealth);
 
         if (currentHealth <=0 && !isDead)
@@ -69,5 +82,5 @@ public class PlayerHealth : MonoBehaviour {
 
         mesh.material.color = Color.blue;
     }
-
+  
 }
